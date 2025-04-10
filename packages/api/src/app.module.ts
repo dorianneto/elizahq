135b08af14ed80e260ca8534c3991f2b9d2bc4dd
Dorian Neto <doriansampaioneto@gmail.com>
2025-04-10 16:17:29 -0230

    feat: get rid of app controller and service

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   imports: [ContactsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
